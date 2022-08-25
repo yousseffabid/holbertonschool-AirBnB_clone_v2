44e064ed9c04f4712bd876fecee618627e449a37
yousseffabid <abidyoussef58@gmail.com>
2022-08-25 13:35:05 +0100

    bash script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,16 +4,16 @@
 sudo apt-get update -y
 sudo apt-get -y install nginx
 
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 sudo touch /data/web_static/releases/test/index.html
 
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 
-echo $'<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tWelcome To Holberton\n\t</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo $'<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "s#root /var/www/html;#root /var/www/html;\n\tlocation /hbnb_static/{\n\t alias /data/web_static/current/;\n}" /etc/nginx/sites-availabe/default
+sudo sed -i "s#root /var/www/html;#root /var/www/html;\n\tlocation /hbnb_static/{\n\t\talias /data/web_static/current/;\n}#g" /etc/nginx/sites-available/default
 sudo service nginx restart
