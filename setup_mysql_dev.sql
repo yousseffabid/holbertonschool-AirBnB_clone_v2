e90841287fd9841cca6a9f72f2b5556b8959325a
yousseffabid <abidyoussef58@gmail.com>
2022-09-09 11:53:09 +0100

    mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,6 +5,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+USE hbnb_dev_db;
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
