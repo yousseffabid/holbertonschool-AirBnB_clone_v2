f2380113446ff8daf8ed06409d82af2d2b865415
yousseffabid <abidyoussef58@gmail.com>
2022-09-10 11:14:26 +0100

    fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     """Displays html page"""
-    states = storage.all(State).values()
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
