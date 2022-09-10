45cc0fc044f7a77babdf19d828975e45d3c1b0d0
yousseffabid <abidyoussef58@gmail.com>
2022-09-10 15:59:40 +0100

    fixed

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,23 +6,28 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
-storage.all()
-
-
-@app.teardown_appcontext
-def teardown_data(self):
-    """
-        reload  data
-    """
-    storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ return all cities """
-    states = storage.all(State)
+    states = storage.all(State).values()
+    cities = list()
 
-    return render_template('8-cities_by_states.html', states=states)
+    for state in states:
+        for city in state.cities:
+            cities.append(city)
+
+    return render_template('8-cities_by_states.html',
+                           states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def teardown_data(exception):
+    """
+        reload  data
+    """
+    storage.close()
 
 
 if __name__ == "__main__":
