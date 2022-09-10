98de5284fea410273bd0579dd60d903d1ee36df6
yousseffabid <abidyoussef58@gmail.com>
2022-09-10 11:25:08 +0100

    fixed

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-from flask import Flask
-from flask import render_template
-import models
+"""
+flask model
+"""
+from flask import Flask, render_template
+from models import storage
 from models.state import State
-from models.city import City
-from os import getenv
-
 app = Flask(__name__)
+storage.all()
 
 
 @app.teardown_appcontext
-def close_sqlalchemy_sess(exception):
-    """remove the current SQLAlchemy Session"""
-    models.storage.close()
+def teardown_data(self):
+    """
+        reload  data
+    """
+    storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """return an html page with list of states"""
-    return render_template('8-cities_by_states.html',
-                           states=models.storage.all(State))
+    """ return all cities """
+    states = storage.all(State)
+
+    return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
