dbe27b2e705303274056d0e46184009094b8e44f
yousseffabid <abidyoussef58@gmail.com>
2022-09-10 13:05:53 +0100

    fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,6 +14,7 @@ def list_states():
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
 def appcontext_teardown(self):
     """use storage for fetching data from the storage engine
     """
